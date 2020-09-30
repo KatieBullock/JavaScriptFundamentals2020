cc6b475c3b8d105ef315394022f4d5a318c990fa
KatieBullock <kathrynleahbullock@gmail.com>
2020-09-29 22:51:03 -0400

    added to exercises/03-functions/functions.js

--- exercises/03-functions/functions.js
+++ exercises/03-functions/functions.js
@@ -4,12 +4,20 @@
  * @example printGreeting("Tim"); // Hello Tim!
  */
 
+const printGreeting = (name) => {
+  return `${"Hello"} ${name}${"!"}`;
+};
+
 /**
  * Create a function named "timesFive". It should multiply a number by 5.
  * @example timesFive(5); // 25
  * @example timesFive(2); // 10
  * @example timesFive(0); // 0
  */
+
+const timesFive = (num) => {
+  return 5 * num;
+};
 
 /**
  * Create a function called "lowerCaseName".
@@ -21,6 +29,14 @@
  * @example lowerCaseName(null); // undefined (should not throw an error)
  */
 
+const lowerCaseName = (str) => {
+  if (str) {
+    return str.toLowerCase();
+  } else {
+    return undefined;
+  }
+};
+
 /**
  * Create a function called "isEvenOrOdd".
  * If given an even number, it should return "even".
@@ -29,11 +45,23 @@
  * @example isEvenOrOdd(3); // odd
  */
 
+const isEvenOrOdd = (num) => {
+  if (num % 2 === 0) {
+    return `${"even"}`;
+  } else {
+    return `${"odd"}`;
+  }
+};
+
 /**
  * Create a function called "countNumberOfDigits".
  * Given an integer, it should return the number of digits in an integer.
  * @example countNumberOfDigits(5000); // 4
  */
+
+const countNumberOfDigits = (num) => {
+  return num.toString().length;
+};
 
 /**
  * Ignore everything below this line. This is for the tests.
