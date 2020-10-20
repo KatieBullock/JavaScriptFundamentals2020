21f5dbd8a13994ea08c535278cc164c45bdf9e4f
KatieBullock <kathrynleahbullock@gmail.com>
2020-10-20 12:59:46 -0400

    added to find.js and reduce.js

--- exercises/14-find/find.js
+++ exercises/14-find/find.js
@@ -5,7 +5,13 @@
  * @param {function} callback
  * @returns {mixed} a single value in the array
  */
-const find = (arr, callback) => {};
+const find = (arr, callback) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return arr[i];
+    }
+  }
+};
 
 /**
  * Find and return the matching user in an array of user objects
@@ -26,7 +32,14 @@ const find = (arr, callback) => {};
  * findUser(users, 1025);
  * // { id: 1025, username:"newyorkfarmer", email: "johndoe@example.com" }
  */
-const findUser = (arr, id) => {};
+const findUser = (arr, id) => {
+  let user = arr.find((person) => {
+    if (person["id"] === id) {
+      return person;
+    }
+  });
+  return user;
+};
 
 module.exports = {
   find,
